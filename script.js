f76c21e14169d2e4bc9c83e1295a21d3c1ff83c4
gabrielsanic <ghenrique018@gmail.com>
2024-11-30 23:24:12 -0300

    Blur adicionado ao carregar imagem para conexões lentas

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
 const range = document.querySelector('input');
 const div = document.querySelector('.imagem');
-const yearDisplaySmallScreen = document.getElementById('ano-display-small-screen');
+const imgElement = div.querySelector('img');
+const anoDisplaySmallScreen = document.getElementById('ano-display-small-screen');
 const dataList = document.getElementById('ano-lista');
 
 function gerarSequencia(base, length, formatter = (value) => value) {
-    return Array.from({ length }, (_, i) => formatter(base + i));
-  }
-  
+  return Array.from({ length }, (_, i) => formatter(base + i));
+}
+
 const images = gerarSequencia(2013, 11, (ano) => `./imagens/${ano}.png`); // Carrega as imagens nomeadas de 2013-2023
 const anos = gerarSequencia(2013, 11);
-  
-// Pré carrega as imagens
+
+// Pré-carrega as imagens
 images.forEach((src) => {
-    const img = new Image();
-    img.src = src;
-  });
+  const img = new Image();
+  img.src = src;
+});
 
 range.max = images.length - 1;
 
-anos.forEach((year, index) => {
-      const option = document.createElement('option');
-      option.value = index;
-      option.label = year;
-      dataList.appendChild(option);
-    });
+anos.forEach((ano, index) => {
+  const option = document.createElement('option');
+  option.value = index;
+  option.label = ano;
+  dataList.appendChild(option);
+});
 
-const updateDisplay = (index) => {
-      div.innerHTML = `<img src="${images[index]}" alt="imagem" />`;
-      yearDisplaySmallScreen.textContent = anos[index];
-    };
+const updateImagemDisplay = (index) => {
+  const newSrc = images[index];
+  imgElement.src = newSrc;
+  imgElement.onload = () => {
+    div.classList.remove('loading'); // Remove o blur após carregar a imagem por completo
+  };
+  div.classList.add('loading'); // Adiciona o blur
+  anoDisplaySmallScreen.textContent = anos[index];
+};
 
 // Inicializa com a primeira imagem e ano
-updateDisplay(0);
+updateImagemDisplay(0);
 
 range.addEventListener('input', (e) => {
-    updateDisplay(e.target.value);
+  updateImagemDisplay(e.target.value);
 });
